9980496694012d0c2ee96a690b2c8badd5338bec
Eric Lekwa <lekwaeric@yahoo.com>
2022-08-25 00:16:41 +0100

    Made changes to the main file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(){
+    println("hello")
+
+    val port = 8080
+
+    val server = embeddedServer(Netty, port){
+        install(ContentNegotiation){
+            jackson{
+                enable(SerializationFeature.INDENT_OUTPUT)
+
+            }
+        }
+        routing {
+            get{
+                context.respond(
+                   mapOf( "Welcome" to "our Cat Hostel")
+                )
+            }
+            get("/{name}"){
+                val name = context.parameters["name"]
+                context.respond(mapOf("Cat name:" to name))
+            }
+        }
+
+    }
+
+    server.start()
+}
